c873006c156a025b2177d47b45f8b0c8371b8266
thiagorizzi <thiagorizzi97@gmail.com>
2022-11-10 23:03:53 -0300

    Avaliação Prática

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/visualization')
 def graph():
 	
-# Students data available in a list of list
+# Importing csv file
    df = pd.read_csv('fire_risk.csv', sep=';', encoding='latin-1')
 
    # Making some changes on the dataframe
@@ -24,7 +24,7 @@ def graph():
 
    df['risk'] = df['risk'].map({'alto': 3, 'médio': 2, 'baixo': 1})
 	
-	# Create Bar chart
+	# Create Histogram chart
    fig = px.histogram(df, x='region', y='risk', color='risk', title='Long-Form Input')
 	
 	# Create graphJSON
